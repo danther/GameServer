618c753b8b89720ea9d439cc84711e440840ba44
danther <danielcorzog@gmail.com>
2015-05-25 01:50:03 +0200

    final version

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,23 @@ var router = express.Router();
 
 /* GET client page */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Grupo 05' });
+  if (isEmpty(req.query)){
+    res.writeHead(302, {"Location": "http://localhost:8080/docs"});
+    res.end();
+
+    //res.render('route', { title: 'Swagger Doc' });
+  } else {
+    res.render('index', { title: 'Grupo 05' });
+  }
 });
+
+function isEmpty(obj) {
+  for(var prop in obj) {
+    if(obj.hasOwnProperty(prop))
+      return false;
+  }
+
+  return true;
+}
 
 module.exports = router;
